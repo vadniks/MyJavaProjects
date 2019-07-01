18c4cc5c8c143dba2cf19022c5c426e9f79a5a3b
vadniks <34599736+vadniks@users.noreply.github.com>
2019-07-01 23:08:37 +0300

    Tab size changed.

--- JavaTheadSync.java
+++ JavaTheadSync.java
@@ -11,45 +11,45 @@ import java.lang.InterruptedException;
  * Console output is 'abc'.
  */
 class JavaTheadSync {
-	private volatile char state = 'a';
+    private volatile char state = 'a';
 
-	public static void main(String[] args) {
-		JavaTheadSync jts = new JavaTheadSync();
-		Thread aa = new Thread(jts::a);
-		Thread bb = new Thread(jts::b);
-		Thread cc = new Thread(jts::c);
-		aa.start();
-		bb.start();
-		cc.start();
-	}
+    public static void main(String[] args) {
+        JavaTheadSync jts = new JavaTheadSync();
+	Thread aa = new Thread(jts::a);
+	Thread bb = new Thread(jts::b);
+	Thread cc = new Thread(jts::c);
+	aa.start();
+	bb.start();
+	cc.start();
+    }
 
-	private synchronized void a() {
-		try {
-			while (state != 'a')
-				wait();
-			System.out.println("a");
-			state = 'b';
-			notify();
-		} catch (InterruptedException ignored) {}
-	}
+    private synchronized void a() {
+	try {
+	    while (state != 'a')
+		wait();
+	    System.out.println("a");
+	    state = 'b';
+	    notify();
+	} catch (InterruptedException ignored) {}
+    }
 
-	private synchronized void b() {
-		try {
-			while (state != 'b')
-				wait();
-			System.out.println("b");
-			state = 'c';
-			notify();
-		} catch (InterruptedException ignored) {}
-	}
+    private synchronized void b() {
+	try {
+	    while (state != 'b')
+                wait();
+	    System.out.println("b");
+	    state = 'c';
+	    notify();
+	} catch (InterruptedException ignored) {}
+    }
 
-	private synchronized void c() {
-		try {
-			while (state != 'c')
-				wait();
-			System.out.println("c");
-			state = 'a';
-			notify();
-		} catch (InterruptedException ignored) {}
-	}	
+    private synchronized void c() {
+	try {
+	    while (state != 'c')
+		wait();
+	    System.out.println("c");
+	    state = 'a';
+	    notify();
+	} catch (InterruptedException ignored) {}
+    }	
 }
